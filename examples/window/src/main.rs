c7d2ecc3f139343d765ab3a211aab8171e9e8304
Yoan Lecoq <yoanlecoq.io@gmail.com>
2017-10-12 23:46:42 +0200

    Log context settings

--- examples/window/src/main.rs
+++ examples/window/src/main.rs
@@ -1,15 +1,20 @@
 extern crate env_logger;
 extern crate dmc;
 extern crate gl;
+#[macro_use]
+extern crate log;
 
 use std::time::Duration;
 use std::thread::sleep;
 use std::ptr;
+use std::ffi::*;
 
 use dmc::display::*;
 use dmc::display::window::Settings as WindowSettings;
 use dmc::option_alternatives::*;
 use dmc::Extent2;
+
+use gl::types::*;
 
 fn main() {
     std::env::set_var("RUST_LOG", "info");
@@ -61,6 +66,99 @@ fn main() {
     });
 
     // TODO: Let's log as much info as we can from the GL context!
+
+    unsafe {
+        let mut ctxflags: GLint = 0;
+        let mut ctxpmask: GLint = 0;
+        let mut depth_bits: GLint = 0;
+        let mut stencil_bits: GLint = 0;
+        let mut double_buffer: GLboolean = 0;
+        let mut stereo_buffers: GLboolean = 0;
+        gl::GetIntegerv(gl::CONTEXT_FLAGS, &mut ctxflags);
+        gl::GetIntegerv(gl::CONTEXT_PROFILE_MASK, &mut ctxpmask);
+        gl::GetFramebufferAttachmentParameteriv(gl::FRAMEBUFFER, gl::DEPTH, 
+                gl::FRAMEBUFFER_ATTACHMENT_DEPTH_SIZE, &mut depth_bits);
+        gl::GetFramebufferAttachmentParameteriv(gl::FRAMEBUFFER, gl::STENCIL, 
+                gl::FRAMEBUFFER_ATTACHMENT_STENCIL_SIZE, &mut stencil_bits);
+        gl::GetBooleanv(gl::DOUBLEBUFFER, &mut double_buffer);
+        gl::GetBooleanv(gl::STEREO, &mut stereo_buffers);
+
+        let ctxflags = ctxflags as GLuint;
+        let ctxpmask = ctxpmask as GLuint;
+
+        let gl_version   = CStr::from_ptr(gl::GetString(gl::VERSION) as _).to_string_lossy();
+        let gl_renderer  = CStr::from_ptr(gl::GetString(gl::RENDERER) as _).to_string_lossy();
+        let gl_vendor    = CStr::from_ptr(gl::GetString(gl::VENDOR) as _).to_string_lossy();
+        let glsl_version = CStr::from_ptr(gl::GetString(gl::SHADING_LANGUAGE_VERSION) as _).to_string_lossy();
+
+        // TODO: report to gl crate.
+        let CONTEXT_FLAG_NO_ERROR_BIT_KHR: GLuint = 0x00000008;
+
+        info!(
+"--- Active OpenGL context settings ---
+    Version             : {}
+    Renderer            : {}
+    Vendor              : {}
+    GLSL version        : {}
+    Profile flags       : {} (0x{:08x})
+    Context flags       : {}{}{}{} (0x{:08x})
+    Double buffering    : {}
+    Stereo buffers      : {}
+    Depth buffer bits   : {}
+    Stencil buffer bits : {}",
+            gl_version, gl_renderer, gl_vendor, glsl_version,
+            if ctxpmask & gl::CONTEXT_CORE_PROFILE_BIT != 0 {
+                "core"
+            } else if ctxpmask & gl::CONTEXT_COMPATIBILITY_PROFILE_BIT != 0 {
+                "compatibility"
+            } else { "" },
+            ctxpmask,
+if ctxflags & gl::CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT != 0 { "forward_compatible " } else {""},
+if ctxflags & gl::CONTEXT_FLAG_DEBUG_BIT != 0 { "debug " } else {""},
+if ctxflags & gl::CONTEXT_FLAG_ROBUST_ACCESS_BIT != 0 { "robust_access " } else {""},
+if ctxflags &     CONTEXT_FLAG_NO_ERROR_BIT_KHR != 0 { "no_error " } else {""},
+            ctxflags,
+            double_buffer, stereo_buffers, depth_bits, stencil_bits
+        );
+    }
+    /*
+    {
+        GLint num_glfmts;
+        glGetIntegerv(GL_NUM_COMPRESSED_TEXTURE_FORMATS, &num_glfmts);
+        GLint *fmts = fe_mem_heapalloc(num_glfmts, GLint, "");
+        fe_dbg_hope(fmts);
+        glGetIntegerv(GL_COMPRESSED_TEXTURE_FORMATS, fmts);
+        fe_logi(TAG, "\n    Compressed texture formats :\n\n");
+        for(i=0 ; i<num_glfmts ; i++)
+            fe_logi(TAG, "0x%.4"PRIx32": %s\n", (int32_t)fmts[i],
+                    fe_gl_tc_format_to_name(fmts[i]));
+        fe_mem_heapfree(fmts);
+    }
+
+    {
+        fe_logi(TAG, "\n    Limits :\n\n");
+        GLint val;
+#define HELPER(CST, req) \
+        glGetIntegerv(GL_MAX_##CST, &val); \
+        fe_logi(TAG, "GL_MAX_%-28s : %d (standard: %d)\n", #CST, (int)val, req)
+        HELPER(RENDERBUFFER_SIZE           ,   1);
+        HELPER(TEXTURE_IMAGE_UNITS         ,   8);
+        HELPER(COMBINED_TEXTURE_IMAGE_UNITS,   8);
+        HELPER(TEXTURE_SIZE                ,  64);
+        HELPER(CUBE_MAP_TEXTURE_SIZE       ,  16);
+        HELPER(VERTEX_ATTRIBS              ,   8);
+        HELPER(VERTEX_TEXTURE_IMAGE_UNITS  ,   0);
+        HELPER(VERTEX_UNIFORM_VECTORS      , 128);
+        HELPER(VARYING_VECTORS             ,   8);
+        HELPER(FRAGMENT_UNIFORM_VECTORS    ,  16);
+#undef HELPER
+        GLint dims[2];
+        glGetIntegerv(GL_MAX_VIEWPORT_DIMS, dims);
+        fe_logi(TAG, "GL_MAX_%-28s : %dx%d\n", "VIEWPORT_DIMS", 
+                (int)dims[0], (int)dims[1]);
+    }
+    */
+
 
     window.set_title("Three");
     unsafe {

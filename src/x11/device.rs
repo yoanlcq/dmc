5f91d43693f44e992e5faafa5538402ca1c4dfe0
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-06-18 08:27:13 +0200

    X11: Draft get_keyboard_state()

--- src/x11/device.rs
+++ src/x11/device.rs
@@ -1,3 +1,6 @@
+use std::slice;
+use super::x11::xinput2 as xi2;
+use super::x11::xlib as x;
 use super::X11SharedContext;
 use device::{
     self,
@@ -53,9 +56,40 @@ impl X11TabletStylusButtonsState {
     }
 }
 
-
 impl X11SharedContext {
     pub fn keyboard_state(&self, keyboard: DeviceID) -> device::Result<KeyboardState> {
+        let x_display = self.lock_x_display();
+        /*
+        // Quoting the man page:
+        // Byte N (from 0) contains the bits for keys 8N to 8N + 7 with the least significant bit in the byte representing key 8N.
+        let mut key_bits: [u8; 32] = [0; 32];
+        let _ = xlib_error::sync_catch(*x_display, || unsafe {
+            x::XQueryKeymap(*x_display, key_bits.as_mut_ptr() as _)
+        })?;
+        unimplemented!{} // FIXME: We're completely ignoring the keyboard ID :(
+        */
+
+        let dev_infos = {
+            let mut nb_infos = 0;
+            let dev_infos = unsafe {
+                xi2::XIQueryDevice(*x_display, unimplemented!{}, &mut nb_infos)
+            };
+            if nb_infos == 0 {
+                return device::failed("XIQueryDevice() returned zero device info");
+            }
+            if dev_infos.is_null() { // FIXME: Can it though?
+                return device::failed("XIQueryDevice() returned NULL");
+            }
+            unsafe {
+                slice::from_raw_parts(dev_infos, nb_infos as _)
+            }
+        };
+
+        unimplemented!{};
+
+        unsafe {
+            xi2::XIFreeDeviceInfo(dev_infos.as_ptr() as *mut _);
+        }
         unimplemented!{}
     }
     pub fn keyboard_keycode_state(&self, keyboard: DeviceID, keycode: Keycode) -> device::Result<KeyState> {
